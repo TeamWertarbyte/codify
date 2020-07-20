058da4727ad619320ebc463a873d785cf94e9977
leMaik <m.marschner@wertarbyte.com>
2020-07-20 15:14:56 +0000

    Deploying to gh-pages from  @ eb40574e9f4a92f8ac121f781c3d6638eb9b92c6 🚀

---
+++ precache-manifest.a86528232438198c5befb25e2d5dd2eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f57c206c15e018aa95e390036396cb2",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "39891761d89bfb1af0761ffb692523b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39529cb80173e13ba9478b165afa88ec",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "281d2b2b762832aa74cc",
+    "url": "/static/css/main.36a08c74.chunk.css"
+  },
+  {
+    "revision": "a38a58a024894eb4559a",
+    "url": "/static/js/2.ce6f548c.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.ce6f548c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281d2b2b762832aa74cc",
+    "url": "/static/js/main.9a23e84e.chunk.js"
+  },
+  {
+    "revision": "f1afa274f8f07b6d9966",
+    "url": "/static/js/runtime-main.6b7fa6cd.js"
+  }
+]);
