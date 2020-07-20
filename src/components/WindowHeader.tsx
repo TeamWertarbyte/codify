5d9f958ced98d11b88186543710a3967a27f36c6
Sascha Becker <s.becker@wertarbyte.com>
2020-07-20 17:06:03 +0200

    Add windows 10 header variant and set as default

--- src/components/WindowHeader.tsx
+++ src/components/WindowHeader.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { createStyles, Theme, WithStyles, withStyles } from "@material-ui/core";
+import { createStyles, IconButton, Theme } from "@material-ui/core";
 import cx from "classnames";
 import { green, grey, red, yellow } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+import { WindowMinimize, WindowMaximize, WindowClose } from "mdi-material-ui";
 
-const styles = ({ spacing }: Theme) =>
+const useStyles = makeStyles(({ spacing }: Theme) =>
   createStyles({
     root: {
       display: "flex",
+    },
+    macOS: {
       backgroundColor: grey[300],
       padding: spacing(0.5),
+    },
+    windows10: {
+      backgroundColor: "#fff",
+      justifyContent: "flex-end",
     },
     dot: {
       borderRadius: "50%",
@@ -25,23 +33,43 @@ const styles = ({ spacing }: Theme) =>
     red: {
       backgroundColor: red[500],
     },
-  });
+  })
+);
 
 interface Props {
   className?: string;
+  variant?: "macOS" | "windows10";
 }
 
-const WindowHeader = ({
-  classes,
-  className,
-}: Props & WithStyles<typeof styles>) => {
-  return (
-    <div className={cx(classes.root, className)}>
-      <div className={cx(classes.dot, classes.red)} />
-      <div className={cx(classes.dot, classes.yellow)} />
-      <div className={cx(classes.dot, classes.green)} />
-    </div>
-  );
+const WindowHeader = ({ className, variant = "windows10" }: Props) => {
+  const classes = useStyles();
+
+  switch (variant) {
+    case "macOS":
+      return (
+        <div className={cx(classes.root, classes.macOS, className)}>
+          <div className={cx(classes.dot, classes.red)} />
+          <div className={cx(classes.dot, classes.yellow)} />
+          <div className={cx(classes.dot, classes.green)} />
+        </div>
+      );
+    case "windows10":
+      return (
+        <div className={cx(classes.root, classes.windows10, className)}>
+          <IconButton size={"small"} style={{ borderRadius: 0 }}>
+            <WindowMinimize />
+          </IconButton>
+          <IconButton size={"small"} style={{ borderRadius: 0 }}>
+            <WindowMaximize />
+          </IconButton>
+          <IconButton size={"small"} style={{ borderRadius: 0 }}>
+            <WindowClose />
+          </IconButton>
+        </div>
+      );
+    default:
+      return <div className={cx(classes.root, className)} />;
+  }
 };
 
-export default withStyles(styles)(WindowHeader);
+export default WindowHeader;
