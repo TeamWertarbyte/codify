2ee1eabe38e8c97ee6d9baa92713313256b51149
Sascha Becker <s.becker@wertarbyte.com>
2020-07-21 11:08:01 +0200

    Add github href

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   createStyles,
+  IconButton,
   Theme,
   Typography,
   WithStyles,
@@ -8,13 +9,15 @@ import {
 } from "@material-ui/core";
 import cx from "classnames";
 import { grey } from "@material-ui/core/colors";
+import { Github } from "mdi-material-ui";
 
 const styles = ({ spacing }: Theme) =>
   createStyles({
     root: {
-      padding: spacing(1, 2),
+      padding: spacing(0, 2),
       backgroundColor: grey[300],
       display: "flex",
+      alignItems: "center",
       justifyContent: "space-between",
     },
   });
@@ -27,6 +30,12 @@ const Header = ({ classes, className }: Props & WithStyles<typeof styles>) => {
   return (
     <div className={cx(classes.root, className)}>
       <Typography variant={"h6"}>Codify</Typography>
+      <IconButton
+        href={"https://github.com/TeamWertarbyte/codify"}
+        target={"_blank"}
+      >
+        <Github />
+      </IconButton>
     </div>
   );
 };
