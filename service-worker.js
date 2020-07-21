2f1d43f4224262f61c6b4d6df169debc16549888
trozanski26 <37703003+trozanski26@users.noreply.github.com>
2020-07-21 11:27:37 +0000

    Deploying to gh-pages from  @ 3d68a76c37146f373703fc5be642fe315a8025e7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://teamwertarbyte.github.io/codify/precache-manifest.0cbdf51d27a33511786b25f8ca5d4e26.js"
+  "https://teamwertarbyte.github.io/codify/precache-manifest.ca5ffa9dc0973c7c49446144974a6cd9.js"
 );
 
 self.addEventListener('message', (event) => {
